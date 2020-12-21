6ba72c0acdd04a4ca38d0e1f77a9d6c7ecced3f7
Kevin Ostafinski <kevinostafinski@gmail.com>
2020-12-21 23:24:40 +0100

    added getColorFormat + hex2Rgb utils

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,8 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { getColorFormat } from './utils/getColorFormat';
+import { hex2Rgb } from './utils/hex2Rgb';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -13,40 +15,27 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('test-ext.rgb2hex', () => {
+	let disposable = vscode.commands.registerCommand('test-ext.hex2rgb', () => {
     // The code you place here will be executed every time your command is executed
-    const editor = vscode.window.activeTextEditor
-    const selection = editor?.selection
-    const selectedText = editor?.document.getText(selection).trim()
+    const editor = vscode.window.activeTextEditor;
+    const selection = editor?.selection;
+    const selectedText = editor?.document.getText(selection).trim();
 
-    function RGBToHex(rgb: String) {
-      // Choose correct separator
-      let sep = rgb.indexOf(",") > -1 ? "," : " ";
-      // Turn "rgb(r,g,b)" into [r,g,b]
-      const rgbArr = rgb.substr(4).split(")")[0].split(sep);
-  
-      let r = (+rgbArr[0]).toString(16),
-          g = (+rgbArr[1]).toString(16),
-          b = (+rgbArr[2]).toString(16);
-    
-      if (r.length == 1)
-        r = "0" + r;
-      if (g.length == 1)
-        g = "0" + g;
-      if (b.length == 1)
-        b = "0" + b;
-    
-      return "#" + r + g + b;
-    }
+    if (selectedText) {
+      const colorFormat = getColorFormat(selectedText);
 
-    if (selectedText?.startsWith('rgb(') && selectedText?.endsWith(')')) {
-      const returnText = RGBToHex(selectedText)
-      vscode.window.showInformationMessage(`${returnText}`);
-      editor?.edit(editBuilder => {
-        if(selection) editBuilder.replace(selection, returnText)
-      })
-    } else {
-      vscode.window.showErrorMessage(`${selectedText} is not an RGB expression`);
+      if (colorFormat) {
+        const hex = hex2Rgb(selectedText);
+
+        if (hex) {
+          vscode.window.showInformationMessage(`${hex}`);
+          editor?.edit(editBuilder => {
+            if(selection) {editBuilder.replace(selection, hex);};
+          });
+        } else {
+          vscode.window.showErrorMessage(`${selectedText} couldn't resolve to an RGB value`);
+        }
+      }
     }
   });
   
---
+++ src/utils/getColorFormat.ts
@@ -0,0 +1,42 @@
+import * as vscode from 'vscode';
+
+export interface ColorTypes {
+  format: string;
+  regex: RegExp;
+}
+
+export const colorTypes: ColorTypes[] = [
+  {
+    format: "hex",
+    regex: /#([\da-f]{3}){1,2}/gi
+  },
+  {
+    format: "rgb",
+    regex: /(rgb)\(\s?\d{1,3}%?\s?(\s?,\s?\d{1,3}%?){2}\s?\)/gi
+  },
+  {
+    format: "rgba",
+    regex: /(rgb)a\(\s?(\d{1,3}%?\s?,\s?){3}(1|0?\.\d+)\s?\)/gi
+  },
+  {
+    format: "hsl",
+    regex: /(hsl)\(\s?\d{1,3}%?\s?(\s?,\s?\d{1,3}%?){2}\s?\)/gi
+  },
+  {
+    format: "hsla",
+    regex: /(hsl)a\(\s?(\d{1,3}%?\s?,\s?){3}(1|0?\.\d+)\s?\)/gi
+  }
+];
+
+
+export function getColorFormat(clr: string) {
+  const matchingType: ColorTypes | undefined = colorTypes.find((type) =>
+    type.regex.exec(clr)
+  );
+
+  if (!matchingType) {
+    vscode.window.showErrorMessage(`Can't resolve color format of ${clr}`);
+    throw new Error(`Can't resolve color format of ${clr}`);
+  }
+  return matchingType.format;
+}
