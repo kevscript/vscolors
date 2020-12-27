74d915f96c7e2d1fb2728586682dcd098e557642
Kevin Ostafinski <kevinostafinski@gmail.com>
2020-12-27 16:35:03 +0100

    updated color Regexes

--- src/utils/getColorType.ts
+++ src/utils/getColorType.ts
@@ -15,26 +15,28 @@ export function getColorType(clr: string) {
     {
       type: "rgb",
       regex: /(rgb)\(\s?\d{1,3}%?\s?(\s?,\s?\d{1,3}%?){2}\s?\)/gi
+      // precise version
+      // regex: /rgb\(\s?(([0-9]|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])|((100|[1-9]?[0-9])\s?\%))\s?\,\s?(([0-9]|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])|((100|[1-9]?[0-9])\s?\%))\s?\,\s?(([0-9]|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])|((100|[1-9]?[0-9])\s?\%))\s?\)/gi    
     },
     {
       type: "rgba",
-      regex: /(rgb)a\(\s?(\d{1,3}%?\s?,\s?){3}(1|0?\.\d+)\s?\)/gi
+      regex: /(rgba)\(\s?(\d{1,3}%?\s?,\s?){3}(1|0?\.\d+)\s?\)/gi
     },
     {
       type: "hsl",
-      regex: /(hsl)\(\s?\d{1,3}%?\s?(\s?,\s?\d{1,3}%?){2}\s?\)/gi
+      regex: /(hsl)\(\s?\d{1,3}\s?(,\s?\d{1,3}%?){2}\s?\)/gi
     },
     {
       type: "hsla",
-      regex: /(hsl)a\(\s?(\d{1,3}%?\s?,\s?){3}(1|0?\.\d+)\s?\)/gi
-    },
-    {
-      type: "hexa",
-      regex: /#([\da-f]{4}){1,2}/gi
+      regex: /(hsla)\(\s?\d{1,3}\s?(,\s?\d{1,3}%?){2}\s?,\s?(1|0?\.\d+)\s?\)/gi
     },
     {
       type: "hex",
       regex: /#([\da-f]{3}){1,2}/gi
+    },
+    {
+      type: "hexa",
+      regex: /#([\da-f]{4}){1,2}/gi
     }
   ];
 
