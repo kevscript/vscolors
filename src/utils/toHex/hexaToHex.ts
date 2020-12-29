f02d179977c3a622984ca082bcbebf9d71a3f260
Kevin Ostafinski <kevinostafinski@gmail.com>
2020-12-30 00:53:59 +0100

    fixed hexaToHsl()

--- src/utils/toHex/hexaToHex.ts
+++ src/utils/toHex/hexaToHex.ts
@@ -4,11 +4,26 @@ import { hexToHex } from './hexToHex';
 export function hexaToHex(clr: string) {
   const arr = clr.split('');
 
-  if ((arr[arr.length - 1] === 'f' || arr[arr.length - 1] === 'F') && (arr[arr.length - 2] === 'f' || arr[arr.length - 2] === 'F')) {
-    const hex = arr.splice(1, 2).join('');
-    return hexToHex(hex);
-  } else {
-    vscode.window.showErrorMessage(`Can't parse HEXA format to HEX because of opacity value.`);
-    throw new Error(`Can't parse HEXA format to HEX because of opacity value.`);
+  // check for hexa full format
+  if (arr.length === 9) {
+    if ((arr[arr.length - 1] === 'f' || arr[arr.length - 1] === 'F') && (arr[arr.length - 2] === 'f' || arr[arr.length - 2] === 'F')) {
+      arr.splice(7, 2);
+      const hex = arr.join('');
+      return hexToHex(hex);
+    } else {
+      vscode.window.showErrorMessage(`Can't parse HEXA format to HEX because of opacity value.`);
+      throw new Error(`Can't parse HEXA format to HEX because of opacity value.`);
+    }
+  } else if (arr.length === 5) {
+    if (arr[arr.length - 1] === 'f' || arr[arr.length - 1] === 'F') {
+      arr.splice(4, 1);
+      const hex = arr.join('');
+      return hexToHex(hex);
+    } else {
+      vscode.window.showErrorMessage(`Can't parse HEXA format to HEX because of opacity value.`);
+      throw new Error(`Can't parse HEXA format to HEX because of opacity value.`);
+    }
   }
+
+  throw new Error(`Error in hexaToHsl().`);
 }
